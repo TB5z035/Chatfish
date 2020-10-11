bc8e44be7374b4b47665501a9b6b702c032a2e92
wzf2000 <nbjcwzf@163.com>
2020-10-11 11:07:25 +0800

    Set user token and connection manager. #17

---
+++ server/connection-manager.js
@@ -0,0 +1,81 @@
+'use strict';
+
+class ConnectionManager {
+    constructor() {
+        this._instance = null
+        this.users = []
+    }
+
+    add_user(id, token) {
+        this.users.push({
+            id: id,
+            token: token
+        })
+    }
+
+    find(id) {
+        return this.users.find(user => {
+            return user.id === id
+        })
+    }
+
+    find_index(id) {
+        return this.users.findIndex(user => {
+            return user.id === id
+        })
+    }
+
+    find_by_token(token) {
+        return this.users.find(user => {
+            return user.token === token
+        })
+    }
+
+    find_index_by_token(token) {
+        return this.users.findIndex(user => {
+            return user.token === token
+        })
+    }
+
+    check_user(id, token) {
+        var user = find(id)
+        if (user) return user.token === token
+        return false
+    }
+
+    set_ws(token, ws) {
+        var index = this.find_index_by_token(token)
+        this.users[index].ws = ws
+        if (this.users[index].timer) {
+            console.log('clear timeout!')
+            clearTimeout(this.users[index].timer)
+            delete this.users[index].timer
+        }
+    }
+
+    get_ws(id) {
+        var index = this.find_index(id)
+        if (index !== -1) return this.users[index].ws
+        else return null
+    }
+    
+    close(id) {
+        var index = this.find_index(id)
+        if (index !== -1) this.users.splice(index, 1)
+    }
+
+    close_ws(id) {
+        var index = this.find_index(id)
+        if (index !== -1) this.users[index].timer = setTimeout(function(ts, index) {
+            ts.users.splice(index, 1)
+            console.log('token expirated!')
+        }, 3 * 60 * 60 * 1000, this, index)
+    }
+
+    static instance() {
+        if (!this._instance) this._instance = new ConnectionManager()
+        return this._instance
+    }
+}
+
+module.exports = ConnectionManager
--- server/websocket_server.js
+++ server/websocket_server.js
@@ -1,12 +1,42 @@
-const ws = require('nodejs-websocket')
 const django_request = require('./django_request')
+const Server = require('ws').Server
+const URL = require('url')
+const cookie_parse = require('cookie').parse
+
+var manager = require('./connection-manager.js').instance()
+
+function get_token(cookie, url) {
+    var cookies = cookie_parse(cookie)
+    if ('token' in cookies)
+        token = cookies.token
+    else {
+        var params = URL.parse(url, true).query
+        token = params.token
+    }
+    return token
+}
+
+function ClientVerify(info) {
+    var token = get_token(info.req.headers.cookie, info.req.url)
+
+    return manager.find_by_token(token)
+}
 
 exports.init_server = function() {
-    return ws.createServer(function(conn) {
-        conn.on("text", function(message) {
+    var wss = new Server({
+        port: 8801,
+        verifyClient: ClientVerify
+    }, function() {
+        console.log('listening')
+    }).on('connection', function(ws, req) {
+        var token = get_token(req.headers.cookie, req.url)
+        manager.set_ws(token, ws)
+        var id = manager.find_by_token(token).id
+        console.log(id, token)
+        ws.on('message', function(message) {
             try {
                 var data = JSON.parse(message)
-                console.log("text received:", JSON.stringify(data))
+                console.log('message received:', JSON.stringify(data))
                 django_request.post('/api/post_data/', data, function(res) {
                     console.log('post_data response: %j', res)
                 }, function(e) {
@@ -16,8 +46,23 @@ exports.init_server = function() {
             catch (e) {
                 console.log('error message: ' + e)
             }
+        }).on('pong', function() {
+            console.log("pong")
+            this.isAlive = true
+        }).on('close', function() {
+            console.log('connection closed...')
+            manager.close_ws(id)
         })
-    }).listen(8801, function() {
-        console.log('listening')
-    })    
+    })
+    const interval = setInterval(function() {
+        wss.clients.forEach(function(ws) {
+            if (ws.isAlive === false) return ws.terminate()
+            ws.isAlive = false
+            ws.ping(function() {})
+        })
+    }, 30000)
+    wss.on('close', function() {
+        clearInterval(interval)
+    })
+    return wss;
 }
