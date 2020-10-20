3fa1f1fbee48c4ce5dece1ff856c49c575378417
励永辉 <2018012167@secoder.net>
2020-10-20 11:48:36 +0800

    remove theme from redux

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux'
 import messages from './messages'
 import socket from './socket'
 import focusUser from './focusUser'
-import theme from './theme'
 
 const chat = combineReducers({
   socket,
   focusUser,
-  messages,
-  theme
+  messages
 })
 
 export default chat
