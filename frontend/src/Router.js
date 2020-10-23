9a528c8fc26ce188078c7571fbebf37df3780e24
励永辉 <2018012167@secoder.net>
2020-10-23 11:45:39 +0800

    set up app test #61

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import DashBoard from './Board/Dashboard'
 import SignInSide from './login'
+import { useSelector } from 'react-redux'
+import { ThemeProvider } from '@material-ui/core'
 
-const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/sign" component={SignInSide}/>
-      <Route exact path="/" component={DashBoard}/>
-    </Switch>
-  </BrowserRouter>
-)
-
+function Router() {
+  const theme = useSelector((state) => state.theme)
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/sign" component={SignInSide}/>
+          <Route exact path="/" component={DashBoard}/>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
 export default Router
