f3b53c5f5c64a02002a1116bb5d4bf4a95e269c6
励永辉 <2018012167@secoder.net>
2020-10-23 14:22:55 +0800

    add more reducer test #61

--- frontend/src/reducers/Reducers.test.js
+++ frontend/src/reducers/Reducers.test.js
@@ -158,15 +158,141 @@ describe('chat reducer', () => {
       theme: null
     }, {
       type: types.SET_MESSAGE_LIST,
-      messageList: [{ user: 'bob', message_list: [], isGroup: 0 }]
+      messageList: [
+        { user: 'bob',
+          message_list:
+              [{ type: 'normal',
+                time: '2020-10-21T06:38:03.063Z',
+                from: 'bob',
+                content: 'hi' }],
+          isGroup: 0 },
+        { user: 'alice',
+          message_list:
+                  [{ type: 'normal',
+                    time: '2019-10-21T06:38:03.063Z',
+                    from: 'alice',
+                    content: 'hello' }],
+          isGroup: 0 }
+      ]
+    })).not.toBeNull()
+    expect(reducer({
+      socket: 'socket',
+      focusUser: 'focusUser',
+      messages: [],
+      myName: 'name',
+      theme: null
+    }, {
+      type: types.SET_MESSAGE_LIST,
+      messageList: [
+        { user: 'bob',
+          message_list:
+                      [],
+          isGroup: 0 },
+        { user: 'alice',
+          message_list:
+                      [],
+          isGroup: 0 }
+      ]
+    })).not.toBeNull()
+    expect(reducer({
+      socket: 'socket',
+      focusUser: 'focusUser',
+      messages: [],
+      myName: 'name',
+      theme: null
+    }, {
+      type: types.SET_MESSAGE_LIST,
+      messageList: [
+        { user: 'bob',
+          message_list:
+              [{ type: 'normal',
+                time: '2019-10-21T06:38:03.063Z',
+                from: 'alice',
+                content: 'hello' }],
+          isGroup: 0 },
+        { user: 'alice',
+          message_list:
+                      [],
+          isGroup: 0 }
+      ]
+    })).not.toBeNull()
+    expect(reducer({
+      socket: 'socket',
+      focusUser: 'focusUser',
+      messages: [],
+      myName: 'name',
+      theme: null
+    }, {
+      type: types.SET_MESSAGE_LIST,
+      messageList: [
+        { user: 'bob',
+          message_list:
+                      [],
+          isGroup: 0 },
+        { user: 'alice',
+          message_list:
+                      [{ type: 'normal',
+                        time: '2019-10-21T06:38:03.063Z',
+                        from: 'alice',
+                        content: 'hello' }],
+          isGroup: 0 }
+      ]
+    })).not.toBeNull()
+  })
+
+  it('should handle setFocusUser', () => {
+    expect(reducer({
+      socket: 'socket',
+      focusUser: 'focusUser',
+      messages: [],
+      myName: 'name',
+      theme: null
+    }, {
+      type: types.SET_FOCUS_USER,
+      username: 'carol'
     })).toEqual(
       {
         socket: 'socket',
-        focusUser: 'focusUser',
-        messages: [{ user: 'bob', message_list: [], isGroup: 0 }],
+        focusUser: 'carol',
+        messages: [],
         myName: 'name',
         theme: null
       }
     )
+  })
+
+  it('should handle messageReceived', () => {
+    expect(reducer({
+      socket: 'socket',
+      focusUser: 'focusUser',
+      messages: [],
+      myName: 'name',
+      theme: null
+    }, {
+      type: types.NEW_MESSAGE_RECEIVE,
+      message: 'hello',
+      author: 'bob'
+    })).not.toBeNull()
+  })
+
+  it('should handle addMessage', () => {
+    expect(reducer({
+      socket: 'socket',
+      focusUser: 'focusUser',
+      messages: [{ user: 'name',
+        message_list: [{ type: 'normal',
+          time: '2019-10-21T06:38:03.063Z',
+          from: 'alice',
+          content: 'hello' }] },
+      { user: 'era',
+        message_list: [] }],
+      myName: 'name',
+      theme: null
+    }, {
+      type: types.NEW_MESSAGE_SEND,
+      message: 'hello',
+      author: 'bob',
+      receiver: 'name'
+    })).not.toBeNull()
   })
 })
