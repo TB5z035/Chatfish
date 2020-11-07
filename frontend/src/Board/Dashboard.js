c43564f3bdd9df03564cadabc29ea12fde61fca0
TB5zhh <654154090@qq.com>
2020-11-07 22:52:50 +0800

    fix video and audio play #67

--- frontend/src/Board/Dashboard.js
+++ frontend/src/Board/Dashboard.js
@@ -481,7 +481,7 @@ export default function Dashboard() {
           setOnline(false)
         }
       } else {
-        history.push('/sign')
+        // history.push('/sign')
       }
     }
     setWebSocket().then()
