a6ed7d27cbbf3199d6e9d690e2f955bab9667730
wzf2000 <nbjcwzf@163.com>
2020-11-11 10:07:49 +0800

    Change test verify to sha256. #81

--- apis/tests.py
+++ apis/tests.py
@@ -53,8 +53,8 @@ class PostTest(TestCase):
     def post_test(self, client, data):
         data = json.dumps(data)
         text_bytes = (data + ' post from ChatFish Server').encode('utf-8')
-        sha1 = hashlib.sha1(text_bytes)
-        key = sha1.hexdigest()
+        sha256 = hashlib.sha256(text_bytes)
+        key = sha256.hexdigest()
         return client.post('/api/post_data/', data = data, HTTP_DATA_KEY = key, content_type = 'application/json')
 
 class WrongPostTest(PostTest):
@@ -71,8 +71,8 @@ class WrongPostTest(PostTest):
         }
         data = json.dumps(data)
         text_bytes = (data + ' this is wrong').encode('utf-8')
-        sha1 = hashlib.sha1(text_bytes)
-        key = sha1.hexdigest()
+        sha256 = hashlib.sha256(text_bytes)
+        key = sha256.hexdigest()
         response = self.client.post('/api/post_data/', data = data, HTTP_DATA_KEY = key, content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
         res_json = json.loads(response.content)
