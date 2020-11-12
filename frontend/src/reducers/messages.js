f70e1df1aca245192232179e1a2f2a3954cf9586
励永辉 <2018012167@secoder.net>
2020-11-12 14:04:24 +0800

    change code style

--- frontend/src/reducers/messages.js
+++ frontend/src/reducers/messages.js
@@ -94,59 +94,6 @@ const messages = (state = initialState, action) => {
 }
 
 const initialState = [
-  // { user: 'testusertestusertest
-  // usertestusertestusertestusert
-  // // estusertestusertestuser', isGroup: 1, message_list: [] },
-  // {
-  //   user: 'testuser',
-  //   isGroup: 1,
-  //   message_list: [
-  //     {
-  //       from: 'Bob', // sender of this message
-  //       time: 1603430942584, // number of milliseconds from 1970.1.1
-  //       content:
-  //         'https://wzf2000-1.oss-cn-hangzhou.aliyuncs.com/ChatFish/image/1604743644455Groundhog.Day.1993.2160p.BluRay.x265.HDR.mUHD-FRDS%20%5BAVC%20480p%5D.mp4',
-  //       mtype: 'abnormal',
-  //       id: 'i am the id of a message'
-  //     }
-  //   ]
-  // },
-  // {
-  //   user: 'testuser1',
-  //   isGroup: 0,
-  //   message_list: [
-  //     {
-  //       from: 'Bob', // sender of this message
-  //       time: 1603430942584, // number of milliseconds from 1970.1.1
-  //       content:
-  //         'https://wzf2000-1.oss-cn-hangzhou.aliyuncs.com/ChatFish/image/1604760255995Alan%20Walker%20-%20Fade%20%5B%E9%AB%98%E8%B4%A8%E9%87%8F%5D%20%5B%E9%AB%98%E8%B4%A8%E9%87%8F%5D%20%5B%E9%AB%98%E8%B4%A8%E9%87%8F%5D.wav',
-  //       mtype: 'abnormal',
-  //       id: 'i am the id of a message'
-  //     },
-  //     {
-  //       from: 'Bob', // sender of this message
-  //       time: 1603430942584, // number of milliseconds from 1970.1.1
-  //       content:
-  //         'https://wzf2000-1.oss-cn-hangzhou.aliyuncs.com/ChatFish/image/1604760189337Threads.jpg',
-  //       mtype: 'abnormal',
-  //       id: 'i am the id of a message'
-  //     }
-  //   ]
-  // }
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser1', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'Alice', isGroup: 0, message_list: [{ mtype: 'normal', time: '2020-10-28T06:20:52.148Z', from: 'Alice', content: 'hello', userInfo: { username: 'Alice', nickname: 'nic2k', email: '' } }], userInfo: { username: 'Alice', nickname: 'nic2k', email: '' } },
-  // { isGroup: 1, userInfo: { username: 'sdked', nickname: 'nickegout', email: '' }, user: 'sdked', message_list: [{ mtype: 'normal', time: '2020-10-28T06:20:52.149Z', from: '2345', content: 'hello', userInfo: { username: 'sdked', nickname: '2345nick', email: '' } }, { mtype: 'normal', time: '2020-10-28T06:41:50.812Z', from: '1234', content: 'hello' , userInfo: { username: 'sdked', nickname: '1234nick', email: '' }}] }
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] },
-  // { user: 'testuser', isGroup: 1, message_list: [] }
 ]
 
 export default messages
