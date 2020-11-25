5f09715ca51a817c415a11f2d321063b00a4d0b2
励永辉 <2018012167@secoder.net>
2020-11-25 19:54:28 +0800

    add more test for reducers

--- frontend/src/fetch/fetch.test.js
+++ frontend/src/fetch/fetch.test.js
@@ -5,6 +5,15 @@ import { postAgreeAddFriend } from './friend/agreeAddFriend'
 import { postAddFriend } from './friend/addFriend'
 import { postAddGroup } from './friend/addGroup'
 import { requireFriendList } from './message/requireFriendList'
+import { postDeleteFriend } from './friend/deleteFriend'
+import { postLeaveGroup } from './friend/leaveGroup'
+import { postDisagreeAddFriend } from './friend/refuseFriend'
+import { postDisagreeAddGroup } from './friend/refuseGroup'
+import { postModifyInfo } from './info/modifyInfo'
+import { postEnterChat } from './message/enterChat'
+import { postUploadMessage } from './message/uploadMessage'
+import { postRecallMessage } from './message/recallMessage'
+import { fetchGroupMember } from './message/fetchGroupMember'
 
 function fetchMock(url, suffix = '') {
   return new Promise((resolve) =>
@@ -76,6 +85,69 @@ it('useStaleRefresh hook runs correctly', async () => {
 it('useStaleRefresh hook runs correctly', async () => {
   act(() => {
     requireFriendList('haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postDeleteFriend('haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postLeaveGroup('haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postDisagreeAddFriend('haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postDisagreeAddGroup('haha', 'haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postModifyInfo('haha', 'haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postEnterChat('haha', 'haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    fetchGroupMember('haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postRecallMessage('haha', 'haha', 'haha')
+  })
+  await act(() => sleep(300))
+})
+
+it('useStaleRefresh hook runs correctly', async () => {
+  act(() => {
+    postUploadMessage('haha')
   })
   await act(() => sleep(300))
 })
